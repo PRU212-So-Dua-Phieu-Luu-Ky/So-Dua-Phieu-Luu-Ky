a23304e8216901de6a1a05bb6bba960282b8eee9
imbatcat <ptdat04@gmail.com>
2025-02-09 10:00:06 +0700

    Enemy attck

--- Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
+++ Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
@@ -7,6 +7,10 @@ public class EnemyMovement : MonoBehaviour
     private Player player;
 
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float playerDetectionRadius = 0.5f;
+
+    [Header(" Debug ")]
+    [SerializeField] private bool showGizmos;
 
     private void Start()
     {
@@ -22,10 +26,34 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        FollowPlayer();
+
+        TryAttack();
+    }
+
+    private void FollowPlayer()
+    {
         Vector2 direction = (player.transform.position - transform.position).normalized;
 
         Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
 
         transform.position = targetPosition;
+    }
+
+    private void TryAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer <= playerDetectionRadius)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!showGizmos) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
     }
 }
